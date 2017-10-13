09fc3c75053fca716286e9871999ebec48513393
Faker Ben Ali <fba@xanweb.com>
2017-10-13 12:32:49 +0100

    update tools file use json_encode and use root namespace

--- concrete/tools/permissions/access/entity/types/submitter.php
+++ concrete/tools/permissions/access/entity/types/submitter.php
@@ -3,11 +3,11 @@
 defined('C5_EXECUTE') or die("Access Denied.");
 use Concrete\Core\Permission\Access\Entity\SubmitterEntity as SubmitterPermissionAccessEntity;
 
-if (\Loader::helper('validation/token')->validate('process')) {
-    $js = \Loader::helper('json');
-    $obj = new \stdClass();
+if (Loader::helper('validation/token')->validate('process')) {
+    $obj = new stdClass();
     $pae = SubmitterPermissionAccessEntity::getOrCreate();
     $obj->peID = $pae->getAccessEntityID();
     $obj->label = $pae->getAccessEntityLabel();
-    print $js->encode($obj);
+    print json_encode($obj);
 }
+
